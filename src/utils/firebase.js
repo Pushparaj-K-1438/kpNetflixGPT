ef629f09f0b814fff1786d2dfdac32a14638aab7
pushparaj <pushparaj@toolyt.com>
2024-06-05 23:23:46 +0530

    firebase setup

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBkaAD9nw3OcSdT1lkou4ZTANaQbvhJxS0",
-  authDomain: "kpnetflixgpt.firebaseapp.com",
-  projectId: "kpnetflixgpt",
-  storageBucket: "kpnetflixgpt.appspot.com",
-  messagingSenderId: "167542917080",
-  appId: "1:167542917080:web:1d4dafc8229a51757414ad",
-  measurementId: "G-2SFFVBNDL9"
+  apiKey: "AIzaSyDn77RVOxDDD6CzkKqJ_rOzVSlo3_LOUi0",
+  authDomain: "kpnetflixgpt-cae22.firebaseapp.com",
+  projectId: "kpnetflixgpt-cae22",
+  storageBucket: "kpnetflixgpt-cae22.appspot.com",
+  messagingSenderId: "247404267474",
+  appId: "1:247404267474:web:76d7ecc6e0358ab6aeaa0b",
+  measurementId: "G-64L551LM7F"
 };
 
 // Initialize Firebase
