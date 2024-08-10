21955f5cc9b4678b4caac0f982e84ba96222645a
pushparaj <pushparaj@toolyt.com>
2024-08-10 21:01:16 +0530

    searched movies fix

--- src/components/dashboard/layout/moviesList/MovieCard.jsx
+++ src/components/dashboard/layout/moviesList/MovieCard.jsx
@@ -2,15 +2,17 @@ import { Link } from "react-router-dom"
 import { MOVIES_IMG_PATH } from "../../../../utils/image"
 
 const MovieCard = ({ posterImg, rating, movieDetId }) => {
+  const imageUrl = posterImg ? MOVIES_IMG_PATH + posterImg : 'path/to/default-image.jpg';
   return (
-    posterImg &&
-    <Link to={'/movies/'+movieDetId}>
-    <div className="flex-none w-32 h-48 transition-transform duration-200 ease transform hover:scale-105 cursor-pointer relative">
-      <img className="w-full h-full object-cover rounded" src={MOVIES_IMG_PATH + posterImg} alt="" />
-      <p className="fixed bottom-2 right-2 bg-red-700 text-white h-8 w-8 flex items-center justify-center text-xs font-bold rounded-full drop-shadow-lg">{rating && parseFloat(rating).toFixed(1)}</p>
-    </div>
+    <Link to={'/movies/' + movieDetId}>
+      <div className="flex-none w-32 h-48 transition-transform duration-200 ease transform hover:scale-105 cursor-pointer relative">
+        <img className="w-full h-full object-cover rounded" src={imageUrl} alt="Movie Poster" />
+        <p className="fixed bottom-2 right-2 bg-red-700 text-white h-8 w-8 flex items-center justify-center text-xs font-bold rounded-full drop-shadow-lg">
+          {rating && parseFloat(rating).toFixed(1)}
+        </p>
+      </div>
     </Link>
-  )
-}
+  );
+};
 
 export default MovieCard
--- src/components/dashboard/layout/moviesList/MovieList.jsx
+++ src/components/dashboard/layout/moviesList/MovieList.jsx
@@ -1,14 +1,23 @@
 import MovieCard from "./MovieCard";
 
-const MovieList = ({ title, movies, isFirst }) => {
+const MovieList = ({ title, movies = [], isFirst }) => {
+  // Ensure `movies` is an array before attempting to map over it
+  if (!Array.isArray(movies)) {
+    movies = [];
+  }
+
   return (
     <div className="flex flex-col gap-2">
       <h3 className={`text-2xl font-semibold ${isFirst ? 'text-white' : 'text-black'}`}>{title}</h3>
       <div className="flex gap-3 overflow-x-scroll scrollbar-hide h-52 items-center">
         {
-          movies?.map((movie) => (
-            <MovieCard key={movie?.id} posterImg={movie?.poster_path} rating={movie?.vote_average} movieDetId={movie?.id} />
-          ))
+          movies.length > 0 ? (
+            movies.map((movie) => (
+              <MovieCard key={movie?.id} posterImg={movie?.poster_path} rating={movie?.vote_average} movieDetId={movie?.id} />
+            ))
+          ) : (
+            <p>No movies available</p>
+          )
         }
       </div>
     </div>
